85a492aabb2b0043fb7757503d5fe324d54d1783
zxbuzzy <zxbuzzy@gmail.com>
2021-10-16 01:58:24 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/about'
+  # get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
