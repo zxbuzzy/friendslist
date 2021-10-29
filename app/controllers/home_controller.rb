d8a935047b7066ae043194c3843b83eef795f06e
zxbuzzy <zxbuzzy@gmail.com>
2021-10-29 19:08:20 +0300

    Add some data from github.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
 class HomeController < ApplicationController
   def index; end
 
   def about
-    @about_me = 'Nikita Andriyannikov'
+    uri = URI('https://api.github.com/users/zxbuzzy')
+    res = JSON.parse(Net::HTTP.get_response(uri).body)
+    @login = res['login']
+    @profile_picture = res['avatar_url']
+    @url = res['html_url']
+    @name = 'Nikita Andriyannikov'
   end
 end
